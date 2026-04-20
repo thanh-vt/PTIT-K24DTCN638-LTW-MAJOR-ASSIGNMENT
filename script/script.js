d447edfbc813cd78cea7eb2d54de6ada76aed746
thanhvt <thanhvt@vissoft.vn>
2026-04-21 03:57:56 +0700

    update #2

--- script/script.js
+++ script/script.js
@@ -1,14 +1,43 @@
-// Trang chủ
-function showMore() {
-  const el = document.getElementById("moreInfo");
-  if (el) el.classList.toggle("hidden");
+
+
+// Tạo header component dùng chung cho các page
+class AppHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+<header>
+  <nav class="navbar">
+    <label style="color: aquamarine">
+      <input type="color" onchange="changeColor(this)">
+      Đổi màu nền
+    </label>
+    <ul class="menu">
+      <li><a href="index.html">Trang chủ</a></li>
+      <li><a href="about.html">Giới thiệu</a></li>
+      <li><a href="project.html">Dự án</a></li>
+    </ul>
+  </nav>
+</header>
+        `;
+    this.setActiveLink();
+  }
+
+  // set trạng thái active của trang
+  setActiveLink() {
+    const currentPath = globalThis.location.pathname.split("/").pop() || "index.html";
+
+    const links = this.querySelectorAll("a");
+
+    links.forEach(link => {
+      const href = link.getAttribute("href");
+
+      if (href === currentPath) {
+        link.classList.add("active");
+      }
+    });
+  }
 }
 
-// Trang dự án
-function toggleProject() {
-  const el = document.getElementById("projectList");
-  if (el) el.classList.toggle("hidden");
-}
+customElements.define("app-header", AppHeader);
 
 // Color picker (nếu có)
 const picker = document.getElementById("colorPicker");
@@ -18,10 +47,15 @@ if (picker) {
     document.body.style.backgroundColor = saved;
     picker.value = saved;
   }
+}
 
-  picker.addEventListener("input", (e) => {
-    const color = e.target.value;
-    document.body.style.backgroundColor = color;
-    localStorage.setItem("bgColor", color);
-  });
+function changeColor(e) {
+  const color = e.value;
+  document.body.style.backgroundColor = color;
+  localStorage.setItem("bgColor", color);
+}
+
+function loadGame(url) {
+  const frame = document.getElementById("gameFrame");
+  frame.src = url;
 }
